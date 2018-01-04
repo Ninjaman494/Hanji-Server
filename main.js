fbeb7062fbd5a2578b1e84ac8c3075f891ddbde8
Akash Eldo <akasheldo@hotmail.com>
2018-01-03 19:14:05 -0500

    Changed default to 0.0.0.0

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.IP || process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.IP || process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function(req, res){
     dic.searchGlosbeKor('듣다',function(translation){
