5cb4135f14e6eddd8cdc1f4ebf9b5d65295f5384
Akash Eldo <akasheldo@hotmail.com>
2018-01-03 19:07:17 -0500

    Changed port and ip addr for OpenShift

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ var dic = require('./dictionary');
 const express = require('express');
 const app = express();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.IP || process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.get('/', function(req, res){
     dic.searchGlosbeKor('듣다',function(translation){
