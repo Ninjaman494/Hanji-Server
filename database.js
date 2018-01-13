757555ad9949debd2d919ae74454c24109fd79c0
Akash Eldo <akasheldo@hotmail.com>
2018-01-13 11:14:06 -0500

    Modified English search query to improve English search.

--- database.js
+++ database.js
@@ -3,7 +3,11 @@ const conString = "postgresql://postgres:password@127.0.0.1:5432/kengdic";
 
 const SEP = "; ";
 const SEARCH_KOR = "SELECT DISTINCT def FROM korean_english WHERE word LIKE '%다' and word LIKE $1";
-const SEARCH_ENG = "SELECT word FROM korean_english WHERE word LIKE '%다' and (UPPER(def) LIKE UPPER($1) OR UPPER(def) LIKE UPPER($2))";
+const SEARCH_ENG = "SELECT word,def FROM korean_english WHERE word LIKE '%다' and (UPPER(def) LIKE UPPER($1) " +
+    "OR UPPER(def) LIKE UPPER($2) " +
+    "OR UPPER(def) like UPPER($3) " +
+    "OR UPPER(def) LIKE upper($4)) LIMIT 10";
+
 
 this.searchKor = function (term, callback) {
     let client = new pg.Client(conString);
@@ -30,7 +34,8 @@ this.searchKor = function (term, callback) {
 this.searchEng = function (term,callback) {
     let client = new pg.Client(conString);
     client.connect();
-    client.query(SEARCH_ENG,['% '+term,term],(err, res) => {
+    let terms = ['% '+term, term, term+",%",'% '+term+",%"];
+    client.query(SEARCH_ENG,terms,(err, res) => {
         let results = [];
         console.log(err ? err.stack : res.rows);
         res.rows.forEach(function(element){
