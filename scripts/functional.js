fcea724e0d33a7415f5b0d1a928c398264ae4cea
Akash Eldo <akasheldo@hotmail.com>
2018-12-22 18:56:19 -0500

    Removed old scripts folder

--- scripts/functional.js
+++
@@ -1,4 +0,0 @@
-if (!Array.prototype.forEach)
-  Array.prototype.forEach = function(callback) {return _.each(this, callback)};
-if (!Array.prototype.reduce)
-  Array.prototype.reduce = function(callback) {return _.reduce(this, callback)};
