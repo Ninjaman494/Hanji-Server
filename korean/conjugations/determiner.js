cbbc0dbfc54f28106e111d83ecea83fd0235d8a4
Akash Eldo <akasheldo@hotmail.com>
2019-01-08 16:20:35 -0500

    Modified Determiner forms to use is_itda_obda function

--- korean/conjugations/determiner.js
+++ korean/conjugations/determiner.js
@@ -3,8 +3,7 @@ let conjugations = {};
 
 conjugations.determiner_present = function(infinitive, regular, isAdj) {
     let stem = conjugator.base(infinitive, regular);
-    if(stem.charAt(stem.length-1) == '있' || stem.charAt(stem.length-1) == '없' || !isAdj){ // special conjugations for these forms
-        //conjugator.reasons.push(`join (${stem} + 는 -> ${stem + '는'})`);
+    if(!isAdj || conjugator.is_itda_obda(infinitive,regular)){ // special conjugations for these forms
         return conjugator.join(stem, '는');
     }else {
         return conjugator.merge(conjugator.base3(infinitive,regular), '은');
@@ -16,8 +15,7 @@ conjugations.determiner_present.tense = 'present';
 conjugations.determiner_present.speechLevel = 'none';
 
 conjugations.determiner_past = function(infinitive, regular, isAdj) {
-    let stem = conjugator.base(infinitive, regular);
-    if(!isAdj && stem.charAt(stem.length-1) != '있' && stem.charAt(stem.length-1) != '없') {
+    if(!isAdj && !conjugator.is_itda_obda(infinitive,regular)) {
         return conjugator.merge(conjugator.base3(infinitive, regular), '은');
     }
 };
@@ -26,11 +24,8 @@ conjugations.determiner_past.type = 'determiner';
 conjugations.determiner_past.tense = 'past';
 conjugations.determiner_past.speechLevel = 'none';
 
-conjugations.determiner_future = function(infinitive, regular, isAdj) {
-    //let stem = conjugator.base(infinitive, regular);
-    //if(!isAdj && stem.charAt(stem.length-1) != '있' && stem.charAt(stem.length-1) != '없') {
+conjugations.determiner_future = function(infinitive, regular) {
     return conjugator.merge(conjugator.base3(infinitive, regular), '을');
-    //}
 };
 conjugations.determiner_future.conjugation = true;
 conjugations.determiner_future.type = 'determiner';
