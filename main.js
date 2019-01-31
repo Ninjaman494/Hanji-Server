cf5ebb4d97d68c0b4c0a44c0e7ec35192d157edf
Akash Eldo <akasheldo@hotmail.com>
2019-01-31 16:04:56 -0500

    Removed depenendency and Changed how port is handled for App Engine

--- main.js
+++ main.js
@@ -23,7 +23,8 @@ const server = new ApolloServer({
     })
 });
 
-server.listen().then( ({url}) => {
+const PORT = process.env.PORT || 4000;
+server.listen({ port: PORT }).then( ({url}) => {
    console.log('Server ready at '+url)
 });
 
