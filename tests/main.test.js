0d81bef00cd940278590976586e67241ff65f21f
akash <akasheldo@hotmail.com>
2020-08-01 10:15:01 -0400

    Created tests for conjugation types, names, and incomplete entries

--- tests/main.test.js
+++ tests/main.test.js
@@ -3,6 +3,7 @@ const { createTestClient } = require('apollo-server-testing');
 const gql = require('graphql-tag');
 const casual = require('casual');
 const DatabaseAPI = require('../datasources/database');
+const ConjugationAPI = require('../datasources/conjugation');
 const resolvers = require('../resolvers');
 const typeDefs = require('../schema');
 
@@ -178,6 +179,94 @@ test('Fetch multiple entries', async () => {
     });
 });
 
+test('Fetch multiple entries where some are incomplete', async () => {
+    const terms = [
+        {
+            id: casual.uuid,
+            term: casual.word,
+            pos: casual.word,
+            definitions: casual.array_of_words(3),
+            examples: [{
+                sentence: casual.sentence,
+                translation: casual.sentence
+            }],
+            antonyms: casual.array_of_words(3),
+            synonyms: casual.array_of_words(3),
+            regular: casual.boolean,
+            note: casual.sentence
+        },
+        {
+            id: casual.uuid,
+            term: casual.word,
+            pos: casual.word,
+            definitions: casual.array_of_words(3),
+            examples: [{
+                sentence: casual.sentence,
+                translation: casual.sentence
+            }],
+        },
+        {
+            id: casual.uuid,
+            term: casual.word,
+            pos: casual.word,
+            definitions: casual.array_of_words(3),
+            note: casual.sentence
+        }
+    ];
+
+    const databaseAPI = new DatabaseAPI();
+    databaseAPI.fetchEntries = () => terms;
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        dataSources: () => ({ databaseAPI}),
+    });
+
+    const { query } = createTestClient(server);
+    const GET_ENTRIES = gql`
+      query getEntries($term: String!) {
+        entries(term: $term) {
+            id,
+            term,
+            pos,
+            definitions,
+            antonyms,
+            synonyms,
+            examples {
+                sentence,
+                translation,
+            },
+            regular,
+            note
+        }
+      }
+    `;
+
+    const res = await query({ query: GET_ENTRIES, variables: { term: "term" } });
+    expect(res.data).not.toBe(null);
+    expect(res.data).not.toBe(undefined);
+    expect(res.data.entries).not.toBe(null);
+    expect(res.data.entries).not.toBe(undefined);
+
+
+    expect(res.data.entries[0]).toEqual(terms[0]);
+    expect(res.data.entries[1]).toEqual({
+        ...terms[1],
+        antonyms: null,
+        synonyms: null,
+        regular: null,
+        note: null,
+    });
+    expect(res.data.entries[2]).toEqual({
+        ...terms[2],
+        examples: null,
+        antonyms: null,
+        synonyms: null,
+        regular: null,
+    });
+});
+
 test('Fetch examples', async () => {
     const examples = [
         {sentence: casual.sentence, translation: casual.sentence},
@@ -210,4 +299,58 @@ test('Fetch examples', async () => {
     expect(res.data.examples).not.toBe(null);
     expect(res.data.examples).not.toBe(undefined);
     expect(res.data.examples).toEqual(examples);
+});
+
+test('Fetch conjugation types', async () => {
+    const types = casual.array_of_words(7);
+
+    const conjugationAPI = new ConjugationAPI();
+    conjugationAPI.fetchConjugationTypes = () => types;
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        dataSources: () => ({ conjugationAPI}),
+    });
+
+    const { query } = createTestClient(server);
+    const GET_TYPES = gql`
+      query getTypes {
+        conjugationTypes
+      }
+    `;
+
+    const res = await query({ query: GET_TYPES });
+    expect(res.data).not.toBe(null);
+    expect(res.data).not.toBe(undefined);
+    expect(res.data.conjugationTypes).not.toBe(null);
+    expect(res.data.conjugationTypes).not.toBe(undefined);
+    expect(res.data.conjugationTypes).toEqual(types);
+});
+
+test('Fetch conjugation names', async () => {
+    const names = casual.array_of_words(7);
+
+    const conjugationAPI = new ConjugationAPI();
+    conjugationAPI.fetchConjugationNames = () => names;
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        dataSources: () => ({ conjugationAPI}),
+    });
+
+    const { query } = createTestClient(server);
+    const GET_NAMES = gql`
+      query getNames {
+        conjugationNames
+      }
+    `;
+
+    const res = await query({ query: GET_NAMES });
+    expect(res.data).not.toBe(null);
+    expect(res.data).not.toBe(undefined);
+    expect(res.data.conjugationNames).not.toBe(null);
+    expect(res.data.conjugationNames).not.toBe(undefined);
+    expect(res.data.conjugationNames).toEqual(names);
 });
