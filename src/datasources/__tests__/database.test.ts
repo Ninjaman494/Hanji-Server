f6965bf86b2536c653a15eb0342ebd0dbad8555a
akash <akasheldo@hotmail.com>
2021-12-21 11:23:00 -0500

    Add tests for read functions

--- src/datasources/__tests__/database.test.ts
+++ src/datasources/__tests__/database.test.ts
@@ -3,9 +3,11 @@ import { MongoClient, ObjectId } from 'mongodb';
 
 describe('DatabaseAPI Datasource', () => {
   let mongoClient: MongoClient;
+  let datasource: DatabaseAPI;
 
   beforeAll(async () => {
     mongoClient = new MongoClient((global as any).__MONGO_URI__);
+    datasource = new DatabaseAPI(mongoClient);
     await mongoClient.connect();
   });
 
@@ -35,7 +37,19 @@ describe('DatabaseAPI Datasource', () => {
       definitions: ['apple'],
     };
 
+    const suggestionId = new ObjectId();
+    const suggestionEntry = {
+      entryID: verbId,
+      antonyms: verbEntry.antonyms,
+      examples: verbEntry.examples,
+    };
+
     beforeAll(async () => {
+      await mongoClient.db('hanji').collection('words').deleteMany({});
+      await mongoClient.db('hanji').collection('words').createIndex({
+        definitions: 'text',
+      });
+
       await mongoClient
         .db('hanji')
         .collection('words')
@@ -51,15 +65,79 @@ describe('DatabaseAPI Datasource', () => {
           _id: nounId,
           ...nounEntry,
         });
+
+      await mongoClient
+        .db('hanji')
+        .collection('words-suggestions')
+        .insertOne({
+          _id: suggestionId,
+          ...suggestionEntry,
+        });
     });
 
-    it('fetches an entry', async () => {
-      const datasource = new DatabaseAPI(mongoClient);
+    test('fetchEntry', async () => {
+      const doc = await datasource.fetchEntry(verbId);
+      expectDocEquals(doc, verbId, verbEntry);
+    });
 
-      const { id, ...rest } = await datasource.fetchEntry(verbId);
+    test('fetchEntries', async () => {
+      const entries = await datasource.fetchEntries(nounEntry.term);
 
-      expect(id).toEqual(verbId.toString());
-      expect(rest).toEqual(verbEntry);
+      expect(entries.length).toEqual(1);
+      expectDocEquals(entries[0], nounId, nounEntry);
+    });
+
+    test('fetchExamples', async () => {
+      const examples = await datasource.fetchExamples(verbId);
+      expect(examples).toEqual(verbEntry.examples);
+    });
+
+    test('searchEnglish', async () => {
+      const { results, cursor } = await datasource.searchEnglish('to go');
+
+      expect(cursor).toEqual(1);
+      expect(results.length).toEqual(1);
+      expectDocEquals(results[0], verbId, { score: 1, ...verbEntry });
+    });
+
+    test('fetchWordoftheDay', async () => {
+      const doc = await datasource.fetchWordoftheDay();
+
+      doc.id === verbId.toString()
+        ? expectDocEquals(doc, verbId, verbEntry)
+        : expectDocEquals(doc, nounId, nounEntry);
+    });
+
+    test('fetchEntrySuggestion', async () => {
+      const doc = await datasource.fetchEntrySuggestion(suggestionId);
+      const { entryID, ...rest } = suggestionEntry;
+
+      expectDocEquals(doc, suggestionId, {
+        applied: false,
+        entryID: entryID.toString(),
+        ...rest,
+      });
+    });
+
+    test('fetchEntrySuggestions', async () => {
+      const suggestions = await datasource.fetchEntrySuggestions();
+
+      const { entryID, ...rest } = suggestionEntry;
+      expect(suggestions.length).toEqual(1);
+      expectDocEquals(suggestions[0], suggestionId, {
+        applied: false,
+        entryID: entryID.toString(),
+        ...rest,
+      });
     });
   });
 });
+
+const expectDocEquals = (
+  { id, ...rest }: Record<string, any>,
+  entryId: ObjectId,
+  entry: any,
+) => {
+  expect(id).toEqual(entryId.toString());
+  expect(rest).toEqual(entry);
+};
