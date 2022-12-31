306883e0a22ce15b06111bd36078179063fc3e75
akash <akasheldo@hotmail.com>
2022-12-31 10:47:50 -0500

    Add notification to wod cron job

--- src/features/wod/refreshWOD.ts
+++ src/features/wod/refreshWOD.ts
@@ -1,5 +1,6 @@
 import { globalCollection, wordsCollection } from 'datasources/databaseWrapper';
 import { EntryDoc } from 'datasources/types';
+import { getMessaging } from 'firebase-admin/messaging';
 
 const refreshWOD = async () => {
   const result = await wordsCollection()
@@ -17,6 +18,29 @@ const refreshWOD = async () => {
     },
     { upsert: true },
   );
+
+  try {
+    const message = {
+      token:
+        'd66KUA5aRdWb-ht4qNACmc:APA91bGI6HeNw1KcQ4qnkMYRhGhesEEjHHcB6LFvronBRomlV_tSknQIis0sb0oruVEpy2irPD57EbqoFuEsQXdDo4WAgZM-SBigT4yDoF651tzLZuX0Hudz9awwoeSCRKqix2yW1Vmz',
+      data: {
+        notifee: JSON.stringify({
+          title: `Today's word is ${newWOD.term}`,
+          body: `Do you know what ${newWOD.term} means?`,
+          android: {
+            channelId: 'wod',
+            pressAction: {
+              id: 'default',
+            },
+          },
+        }),
+      },
+    };
+    const response = await getMessaging().send(message);
+    console.log(`FCM successful: ${response}`);
+  } catch (err) {
+    console.error(`FCM failed: ${err}`);
+  }
 };
 
 export default refreshWOD;
