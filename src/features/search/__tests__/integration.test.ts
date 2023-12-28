3b17681483a33ca65b60c3271b598bdc0ef08500
Ninjaman494 <akasheldo@hotmail.com>
2023-12-28 11:10:22 -0500

    Migrate to MongoDB Atlas Search (#179)
    * Migrate from text search to atlas search
    
    * Wrote tests
    
    * Fixed search resolver test

--- src/features/search/__tests__/integration.test.ts
+++ src/features/search/__tests__/integration.test.ts
@@ -1,7 +1,8 @@
 import { ApolloServer } from '@apollo/server';
 import gql from 'graphql-tag';
 import { Entry, General, Search } from 'features';
-import { omit } from 'lodash';
+import { defaults, omit } from 'lodash';
+import * as databaseWrapper from 'datasources/databaseWrapper';
 import {
   ENTRIES,
   executeOperation,
@@ -9,6 +10,7 @@ import {
   teardownDB,
 } from 'tests/utils';
 import resolvers from '../resolvers';
+import { entryReducer } from 'features/utils';
 
 const query = gql`
   query Search($query: String!) {
@@ -39,11 +41,12 @@ const server = new ApolloServer({
 });
 
 describe('search feature', () => {
-  beforeAll(async () => await setupMockDB());
-
-  afterAll(async () => await teardownDB());
-
   it('handles search queries in English', async () => {
+    const entries = ENTRIES.map(entryReducer);
+    jest.spyOn(databaseWrapper, 'wordsCollection').mockReturnValueOnce({
+      aggregate: () => ({ map: () => ({ toArray: () => entries }) }),
+    } as any);
+
     const { errors, data } = await executeOperation(server, query, {
       query: 'to go',
     });
@@ -51,31 +54,22 @@ describe('search feature', () => {
     expect(errors).toBeUndefined();
     expect(data).toBeDefined();
     expect(data.search).toEqual({
-      cursor: 2,
-      results: [
-        {
-          id: ENTRIES[1]._id.toString(),
-          ...omit(ENTRIES[1], ['_id']),
+      cursor: 3,
+      results: entries.map((e) =>
+        defaults(e, {
           antonyms: null,
           synonyms: null,
           examples: null,
           note: null,
           regular: null,
-        },
-        {
-          id: ENTRIES[2]._id.toString(),
-          ...omit(ENTRIES[2], ['_id']),
-          antonyms: null,
-          synonyms: null,
-          examples: null,
-          note: null,
-          regular: null,
-        },
-      ],
+        }),
+      ),
     });
   });
 
   it('handles search queries in Korean', async () => {
+    await setupMockDB();
+
     const { errors, data } = await executeOperation(server, query, {
       query: '가다',
     });
@@ -96,5 +90,7 @@ describe('search feature', () => {
         },
       ],
     });
+
+    await teardownDB();
   });
 });
--- src/features/search/__tests__/resolvers.test.ts
+++ src/features/search/__tests__/resolvers.test.ts
@@ -3,6 +3,7 @@ jest.mock('features/autocorrect/autocorrect');
 import { omit } from 'lodash';
 import { ObjectId } from 'mongodb';
 import { setupMockDB, teardownDB } from 'tests/utils';
+import * as databaseWrapper from 'datasources/databaseWrapper';
 import resolvers from '../resolvers';
 import { findCorrection } from 'features/autocorrect/autocorrect';
 
@@ -59,54 +60,82 @@ describe('search resolver', () => {
   });
 
   describe('English', () => {
+    const toArray = jest.fn();
+    const aggregate = jest.fn().mockReturnValue({ map: () => ({ toArray }) });
+    const verifySearchQuery = (query: string, cursor = 0) => {
+      expect(aggregate).toHaveBeenCalledWith([
+        {
+          $search: {
+            index: 'default',
+            text: { query, path: 'definitions' },
+          },
+        },
+        { $skip: cursor },
+        { $limit: 20 },
+      ]);
+    };
+
+    beforeEach(() => {
+      jest
+        .spyOn(databaseWrapper, 'wordsCollection')
+        .mockReturnValue({ aggregate } as any);
+    });
+
     it('resolves search queries', async () => {
+      const query = 'kimchi';
+      toArray.mockReturnValueOnce([entries[0]]);
+
       const { results, cursor } = await (resolvers.Query.search as any)(null, {
-        query: 'kimchi',
+        query,
       });
 
-      const { _id, ...rest } = entries[0];
-      expect(results.length).toEqual(1);
+      verifySearchQuery(query);
+      expect(results).toEqual([entries[0]]);
       expect(cursor).toEqual(1);
-      expect(omit(results[0], ['score'])).toEqual({
-        id: _id.toString(),
-        ...rest,
-      });
     });
 
     it('can paginate search results', async () => {
+      const query = 'to go';
+
       // First page
+      let mockedReturnValue = entries.slice(2);
+      toArray.mockReturnValueOnce(mockedReturnValue);
       const { results, cursor } = await (resolvers.Query.search as any)(null, {
-        query: 'to go',
+        query,
       });
 
-      const toGoEntry = omit(entries[1], ['score', '_id']);
-      const toGoOutEntry = omit(entries[2], ['score', '_id']);
-
-      expect(results.length).toEqual(20);
+      verifySearchQuery(query);
+      expect(results).toEqual(mockedReturnValue);
       expect(cursor).toEqual(20);
-      expect(omit(results[0], ['score', 'id'])).toEqual(toGoEntry);
-      expect(omit(results[1], ['score', 'id'])).toEqual(toGoOutEntry);
 
       // Second page
+      mockedReturnValue = entries.slice(21);
+      toArray.mockReturnValueOnce(mockedReturnValue);
       const { results: paginatedResults, cursor: paginatedCursor } = await (
         resolvers.Query.search as any
-      )(null, { query: 'to go', cursor });
+      )(null, { query, cursor });
 
-      expect(paginatedResults.length).toEqual(1);
+      verifySearchQuery(query, cursor);
+      expect(paginatedResults).toEqual(mockedReturnValue);
       expect(paginatedCursor).toEqual(21);
-      expect(omit(paginatedResults[0], ['score', 'id'])).toEqual(toGoOutEntry);
 
       // Last page, should be empty
+      mockedReturnValue = [];
+      toArray.mockReturnValueOnce(mockedReturnValue);
       const { results: noResults, cursor: noCursor } = await (
         resolvers.Query.search as any
-      )(null, { query: 'to go', cursor: paginatedCursor });
+      )(null, { query, cursor: paginatedCursor });
 
-      expect(noResults.length).toEqual(0);
+      verifySearchQuery(query, paginatedCursor);
+      expect(noResults).toEqual(mockedReturnValue);
       expect(noCursor).toEqual(-1);
     });
   });
 
   describe('Korean', () => {
+    // Clear mocks used in English tests and go back to actual implementation
+    beforeAll(jest.restoreAllMocks);
+
     it('resolves search queries', async () => {
       const { results, cursor } = await (resolvers.Query.search as any)(null, {
         query: '김지',
